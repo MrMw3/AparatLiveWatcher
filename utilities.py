7ccd03e2e5de3fa89c7c42a43932f5494bb1d3ed
Reza Soleimani <myviralbrain@gmail.com>
2024-03-06 15:40:13 +0330

    fix dismissed "w" in save page method

--- utilities.py
+++ utilities.py
@@ -14,7 +14,7 @@ def validate_url(url):
     
 def save_page_source(page_source):
     try:
-        with open("logs/source.txt", encoding='utf-8') as file:
+        with open("logs/source.txt", "w", encoding='utf-8') as file:
             file.write(page_source)
             print("[+] Page source saved in logs/source.txt Successfully/")
     except Exception as ex:
